3a720caeac7c2541a3667352738cf0bfa5bfb55e
Cristian1007 <cristian.diaz2187@utc.edu.ec>
2023-11-13 21:16:30 -0500

    version1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,100 +1,360 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Cooperativa Mi Tierra</title>
+	</head>
+	<body>
 
-	<style type="text/css">
+		<!-- ***** Preloader Start ***** -->
+		<div id="preloader">
+				<div class="jumper">
+						<div></div>
+						<div></div>
+						<div></div>
+				</div>
+		</div>
+		<!-- ***** Preloader End ***** -->
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+		<!-- Header -->
+		<div class="sub-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-xs-12">
+						<ul class="left-info">
+							<li><a href="#"><i class="fa fa-clock-o"></i>Mon-Fri 09:00-17:00</a></li>
+							<li><a href="#"><i class="fa fa-phone"></i>090-080-0760</a></li>
+						</ul>
+					</div>
+					<div class="col-md-4">
+						<ul class="right-icons">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+		<!-- Page Content -->
+		<!-- Banner Starts Here -->
+		<div class="main-banner header-text" id="top">
+				<div class="Modern-Slider">
+					<!-- Item -->
+					<div class="item item-1">
+						<div class="img-fill">
+								<div class="text-content">
+									<h6>we are ready to help you</h6>
+									<h4>Financial Analysis<br>&amp; Consulting</h4>
+									<p>This finance HTML template is 100% free of charge provided by TemplateMo for everyone. This is a multiple-page version with different HTML pages. <a href="one-page.html">One-page version</a> is available as of 2021 February.</p>
+									<a href="contact.html" class="filled-button">contact us</a>
+								</div>
+						</div>
+					</div>
+					<!-- // Item -->
+					<!-- Item -->
+					<div class="item item-2">
+						<div class="img-fill">
+								<div class="text-content">
+									<h6>we are here to support you</h6>
+									<h4>Accounting<br>&amp; Management</h4>
+									<p>You are allowed to use this template for your company websites. You are NOT allowed to re-distribute this template ZIP file on any template download website. Please contact TemplateMo for more detail.</p>
+									<a href="services.html" class="filled-button">our services</a>
+								</div>
+						</div>
+					</div>
+					<!-- // Item -->
+					<!-- Item -->
+					<div class="item item-3">
+						<div class="img-fill">
+								<div class="text-content">
+									<h6>we have a solid background</h6>
+									<h4>Market Analysis<br>&amp; Statistics</h4>
+									<p>You can download, edit and use this layout for your business website. Phasellus lacinia ac sapien vitae dapibus. Mauris ut dapibus velit cras interdum nisl ac urna tempor mollis.</p>
+									<a href="about.html" class="filled-button">learn more</a>
+								</div>
+						</div>
+					</div>
+					<!-- // Item -->
+				</div>
+		</div>
+		<!-- Banner Ends Here -->
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-		text-decoration: none;
-	}
+		<div class="request-form">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h4>Request a call back right now ?</h4>
+						<span>Mauris ut dapibus velit cras interdum nisl ac urna tempor mollis.</span>
+					</div>
+					<div class="col-md-4">
+						<a href="contact.html" class="border-button">Contact Us</a>
+					</div>
+				</div>
+			</div>
+		</div>
 
-	a:hover {
-		color: #97310e;
-	}
+		<div class="services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="section-heading">
+							<h2>Financial <em>Services</em></h2>
+							<span>Aliquam id urna imperdiet libero mollis hendrerit</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="service-item">
+							<img src="assets/images/service_01.jpg" alt="">
+							<div class="down-content">
+								<h4>Digital Currency</h4>
+								<p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
+								<a href="" class="filled-button">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="service-item">
+							<img src="assets/images/service_02.jpg" alt="">
+							<div class="down-content">
+								<h4>Market Analysis</h4>
+								<p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
+								<a href="" class="filled-button">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="service-item">
+							<img src="assets/images/service_03.jpg" alt="">
+							<div class="down-content">
+								<h4>Historical Data</h4>
+								<p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
+								<a href="" class="filled-button">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+		<div class="fun-facts">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="left-content">
+							<span>Lorem ipsum dolor sit amet</span>
+							<h2>Our solutions for your <em>business growth</em></h2>
+							<p>Pellentesque ultrices at turpis in vestibulum. Aenean pretium elit nec congue elementum. Nulla luctus laoreet porta. Maecenas at nisi tempus, porta metus vitae, faucibus augue.
+							<br><br>Fusce et venenatis ex. Quisque varius, velit quis dictum sagittis, odio velit molestie nunc, ut posuere ante tortor ut neque.</p>
+							<a href="" class="filled-button">Read More</a>
+						</div>
+					</div>
+					<div class="col-md-6 align-self-center">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="count-area-content">
+									<div class="count-digit">945</div>
+									<div class="count-title">Work Hours</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="count-area-content">
+									<div class="count-digit">1280</div>
+									<div class="count-title">Great Reviews</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="count-area-content">
+									<div class="count-digit">578</div>
+									<div class="count-title">Projects Done</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="count-area-content">
+									<div class="count-digit">26</div>
+									<div class="count-title">Awards Won</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
+		<div class="more-info">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="more-info-content">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="left-image">
+										<img src="assets/images/more-info.jpg" alt="">
+									</div>
+								</div>
+								<div class="col-md-6 align-self-center">
+									<div class="right-content">
+										<span>Who we are</span>
+										<h2>Get to know about <em>our company</em></h2>
+										<p>Curabitur pulvinar sem a leo tempus facilisis. Sed non sagittis neque. Nulla conse quat tellus nibh, id molestie felis sagittis ut. Nam ullamcorper tempus ipsum in cursus<br><br>Praes end at dictum metus. Morbi id hendrerit lectus, nec dapibus ex. Etiam ipsum quam, luctus eu egestas eget, tincidunt</p>
+										<a href="#" class="filled-button">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-	#body {
-		margin: 0 15px 0 15px;
-		min-height: 96px;
-	}
+		<div class="testimonials">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="section-heading">
+							<h2>What they say <em>about us</em></h2>
+							<span>testimonials from our greatest clients</span>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="owl-testimonials owl-carousel">
 
-	p {
-		margin: 0 0 10px;
-		padding:0;
-	}
+							<div class="testimonial-item">
+								<div class="inner-content">
+									<h4>George Walker</h4>
+									<span>Chief Financial Analyst</span>
+									<p>"Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem sit amet quam. Pellentesque in sagittis lacus."</p>
+								</div>
+								<img src="http://placehold.it/60x60" alt="">
+							</div>
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
+							<div class="testimonial-item">
+								<div class="inner-content">
+									<h4>John Smith</h4>
+									<span>Market Specialist</span>
+									<p>"In eget leo ante. Sed nibh leo, laoreet accumsan euismod quis, scelerisque a nunc. Mauris accumsan, arcu id ornare malesuada, est nulla luctus nisi."</p>
+								</div>
+								<img src="http://placehold.it/60x60" alt="">
+							</div>
 
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
+							<div class="testimonial-item">
+								<div class="inner-content">
+									<h4>David Wood</h4>
+									<span>Chief Accountant</span>
+									<p>"Ut ultricies maximus turpis, in sollicitudin ligula posuere vel. Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique."</p>
+								</div>
+								<img src="http://placehold.it/60x60" alt="">
+							</div>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+							<div class="testimonial-item">
+								<div class="inner-content">
+									<h4>Andrew Boom</h4>
+									<span>Marketing Head</span>
+									<p>"Curabitur sollicitudin, tortor at suscipit volutpat, nisi arcu aliquet dui, vitae semper sem turpis quis libero. Quisque vulputate lacinia nisl ac lobortis."</p>
+								</div>
+								<img src="http://placehold.it/60x60" alt="">
+							</div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<div class="callback-form">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="section-heading">
+							<h2>Request a <em>call back</em></h2>
+							<span>Etiam suscipit ante a odio consequat</span>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="contact-form">
+							<form id="contact" action="" method="post">
+								<div class="row">
+									<div class="col-lg-4 col-md-12 col-sm-12">
+										<fieldset>
+											<input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
+										</fieldset>
+									</div>
+									<div class="col-lg-4 col-md-12 col-sm-12">
+										<fieldset>
+											<input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Address" required="">
+										</fieldset>
+									</div>
+									<div class="col-lg-4 col-md-12 col-sm-12">
+										<fieldset>
+											<input name="subject" type="text" class="form-control" id="subject" placeholder="Subject" required="">
+										</fieldset>
+									</div>
+									<div class="col-lg-12">
+										<fieldset>
+											<textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+										</fieldset>
+									</div>
+									<div class="col-lg-12">
+										<fieldset>
+											<button type="submit" id="form-submit" class="border-button">Send Message</button>
+										</fieldset>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+		<div class="partners">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="owl-partners owl-carousel">
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
-	</div>
+							<div class="partner-item">
+								<img src="assets/images/client-01.png" title="1" alt="1">
+							</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+							<div class="partner-item">
+								<img src="assets/images/client-01.png" title="2" alt="2">
+							</div>
 
-</body>
-</html>
+							<div class="partner-item">
+								<img src="assets/images/client-01.png" title="3" alt="3">
+							</div>
+
+							<div class="partner-item">
+								<img src="assets/images/client-01.png" title="4" alt="4">
+							</div>
+
+							<div class="partner-item">
+								<img src="assets/images/client-01.png" title="5" alt="5">
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="sub-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p>Copyright &copy; 2020 Financial Business Co., Ltd.
+
+						- Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a><br>
+						Distributed By: <a rel="nofollow noopener" href="https://themewagon.com" target="_blank">ThemeWagon</a>
+					</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</body>
